dd1b925cd880fe5161a5fc8b8c736514a23dbfc5
QueenterJuma <jumahqueenter@gmail.com>
2023-04-20 16:47:10 +0300

    Add Homeinfo components

---
+++ src/components/Homeinfo.js
@@ -0,0 +1,61 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { BsFillArrowRightCircleFill } from 'react-icons/bs';
+import {
+  oceaniaCountries,
+  filterByCapital,
+} from '../redux/country/FetchCountry';
+
+const HomeInfo = () => {
+  const dispatch = useDispatch();
+  const { Data, success, error, loading, oceania, FilterTerm } = useSelector(
+    (state) => state.country
+  );
+  useEffect(() => {
+    dispatch(oceaniaCountries());
+    if (oceania) {
+      dispatch(filterByCapital(oceania));
+    }
+    if (error) {
+      dispatch(error);
+    }
+  }, [dispatch, error, success, oceania]);
+  const data = Data.filter((item) =>
+    item.name.common.toLowerCase().includes(FilterTerm)
+  );
+  // const data = Array.isArray(Data)
+  //   ? Data.filter((item) => item.name.common.toLowerCase().includes(FilterTerm))
+  //   : [];
+  // console.log(Data);
+  return (
+    <div className="homeinfo-container">
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        data.length > 0 &&
+        data.map((item) => (
+          <Link
+            className="homeinfo-card"
+            key={item.name.common}
+            to={`/${item.cioc}`}
+          >
+            <p className="next">
+              <BsFillArrowRightCircleFill className="next-icon" />
+            </p>
+            <img src={item.flags.png} alt={item.flags.alt} className="-image" />
+            <div className="homeinfo-content">
+              <h3>{item.name.common} </h3>
+              <p>
+                {' '}
+                <span>{item.population}</span>
+              </p>
+            </div>
+          </Link>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default HomeInfo;
