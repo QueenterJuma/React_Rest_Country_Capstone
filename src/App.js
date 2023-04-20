d765dbb831a3b7fa2b924f9e4c694e0424f051e0
QueenterJuma <jumahqueenter@gmail.com>
2023-04-20 16:22:43 +0300

    Add update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Countryinfo from './components/Countryinfo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/:code" element={<Countryinfo />} />
+      </Routes>
     </div>
   );
 }
