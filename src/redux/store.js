f30131a455e58a9f2e703a2be628948ac9d9107c
QueenterJuma <jumahqueenter@gmail.com>
2023-04-21 17:40:03 +0300

    Add Update in store.js file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-// import rootReducer from './reducers';
 import countriesReducer from './country/CountryInfoSlice';
-
-// const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const store = configureStore({
   reducer: {
