31aab20087fef4a70bbc5fdc5dcfe43ec767c150
QueenterJuma <jumahqueenter@gmail.com>
2023-04-22 05:17:59 +0300

    Add json and replace axios in FetchCountry,js file

--- src/redux/country/FetchCountry.js
+++ src/redux/country/FetchCountry.js
@@ -1,46 +1,40 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-
-// const url = 'https://restcountries.com/v3.1/region/oceania';
-// 'https://restcountries.com/v3.1/region/africa';
 
 export const oceaniaCountries = createAsyncThunk(
   'countries/oceania',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('https://restcountries.com/v3.1/all');
-      return response.data;
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      const data = await response.json();
+      return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
   },
 );
-
-// export default oceaniaCountries;
-
 export const filterByCode = createAsyncThunk(
   'countries/filterByCode',
   async (code, thunkAPI) => {
     try {
-      const response = await axios.get(
+      const response = await fetch(
         `https://restcountries.com/v3.1/alpha?codes=${code}`,
       );
-      return response.data;
+      const data = await response.json();
+      return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
   },
 );
-
 export const filterByCapital = createAsyncThunk(
   'countries/filterByCapital',
   async (capital, thunkAPI) => {
     try {
-      const response = await axios.get(
+      const response = await fetch(
         `https://restcountries.com/v3.1/capital/${capital}`,
       );
-      // const data = await response.axios();
-      return response.data;
+      const data = await response.json();
+      return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
